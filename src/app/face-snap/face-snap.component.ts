4c8f1e1e45a4ebb62a6e5f1c5eba5d3b16bba5b5
Willysmile <fabienrabillard@yahoo.fr>
2022-11-29 15:20:20 +0100

    First component created

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
+title!: string;
+description!: string;
+createdDate!: Date;
+snaps!: number;
 
+imageUrl!: string;
+
+ngOnInit() {
+  this.title = 'Bébé Tigre Blanc';
+  this.description = 'Une peluche au Zoo de Pessac';
+  this.createdDate = new Date();
+  this.snaps = 7;
+  this.imageUrl = 'https://live.staticflickr.com/3428/3808485369_41e53c9333_h.jpg'
+}
 }
