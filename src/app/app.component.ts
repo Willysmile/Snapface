c5bf8b0749605678daeb8a0f78dfcfac18a04a8f
Willysmile <fabienrabillard@yahoo.fr>
2022-11-30 00:22:14 +0100

    End part 2 of course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FaceSnap} from "./models/face-snap.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Bébé Tigre Blanc',
+      'Une peluche au Zoo de Pessac',
+      'https://live.staticflickr.com/3428/3808485369_41e53c9333_h.jpg',
+      new Date(),
+      7,
+    );
+    this.myOtherSnap = new FaceSnap(
+      'Petit Panda',
+      'Une autre peluche au Zoo de Pessac',
+      'https://live.staticflickr.com/2670/3808442547_1e9b58b3ed_h.jpg',
+      new Date(),
+      5,
+    );
+    this.myLastSnap = new FaceSnap(
+      'Ara Chloroptère',
+      'Un Ara au Zoo de Pessac',
+      'https://live.staticflickr.com/3534/3808455211_8571d30550.jpg',
+      new Date(),
+      2,
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,39 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {FaceSnap} from "../models/face-snap.model";
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit{
-title!: string;
-description!: string;
-createdDate!: Date;
-snaps!: number;
-snapped!:boolean
-buttonText!:string;
-imageUrl!: string;
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
 
-ngOnInit() {
-  this.title = 'Bébé Tigre Blanc';
-  this.description = 'Une peluche au Zoo de Pessac';
-  this.createdDate = new Date();
-  this.snaps = 7;
-  this.imageUrl = 'https://live.staticflickr.com/3428/3808485369_41e53c9333_h.jpg'
-  this.snapped = false;
-  this.buttonText = "Oh Snap!";
-}
-onSnap(){
- if (!this.snapped) {
-   this.snapped = !this.snapped;
-   this.snaps++;
-   this.buttonText = "Oops, un Snap!";
- }
- else {
-   this.snapped = !this.snapped;
-   this.snaps--;
-   this.buttonText = "Oh Snap!";
- }
 
-}
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  snapped!: boolean
+  buttonText!: string;
+  imageUrl!: string;
+
+  ngOnInit() {
+    
+    this.snapped = false;
+    this.buttonText = "Oh Snap!";
+  }
+
+  onSnap() {
+    if (!this.snapped) {
+      this.snapped = !this.snapped;
+      this.faceSnap.snaps++;
+      this.buttonText = "Oops, un Snap!";
+    } else {
+      this.snapped = !this.snapped;
+      this.faceSnap.snaps--;
+      this.buttonText = "Oh Snap!";
+    }
+
+  }
 }
