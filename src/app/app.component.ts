0f836477c446c2c2ca13c6956e78c14d43ace59c
Willysmile <fabienrabillard@yahoo.fr>
2022-12-03 17:57:24 +0100

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,34 @@ import {FaceSnap} from "./models/face-snap.model";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Bébé Tigre Blanc',
-      description: 'Une peluche au Zoo de Pessac',
-      imageUrl: 'https://live.staticflickr.com/3428/3808485369_41e53c9333_h.jpg',
-      createdDate: new Date(),
-      snaps: 7,
-      location: 'Pessac'
-    };
-    this.myOtherSnap = {
-      title: 'Petit Panda',
-      description: 'Une autre peluche au Zoo de Pessac',
-      imageUrl: 'https://live.staticflickr.com/2670/3808442547_1e9b58b3ed_h.jpg',
-      createdDate: new Date(),
-      snaps: 5,
-      location: 'Bordeaux'
-    };
-    this.myLastSnap = {
-      title: 'Ara Chloroptère',
-      description: 'Un Ara au Zoo de Pessac',
-      imageUrl: 'https://live.staticflickr.com/3534/3808455211_8571d30550.jpg',
-      createdDate: new Date(),
-      snaps: 2,
-    };
+    this.faceSnaps = [
+      {
+        title: 'Bébé Tigre Blanc',
+        description: 'Une peluche au Zoo de Pessac',
+        imageUrl: 'https://live.staticflickr.com/3428/3808485369_41e53c9333_h.jpg',
+        createdDate: new Date(),
+        snaps: 7,
+        location: 'Pessac'
+      },
+      {
+        title: 'Petit Panda',
+        description: 'Une autre peluche au Zoo de Pessac',
+        imageUrl: 'https://live.staticflickr.com/2670/3808442547_1e9b58b3ed_h.jpg',
+        createdDate: new Date(),
+        snaps: 5,
+        location: 'Bordeaux'
+      },
+      {
+        title: 'Ara Chloroptère',
+        description: 'Un Ara au Zoo de Pessac',
+        imageUrl: 'https://live.staticflickr.com/3534/3808455211_8571d30550.jpg',
+        createdDate: new Date(),
+        snaps: 2,
+      }
+    ]
+
   }
 }
